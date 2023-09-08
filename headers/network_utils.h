786c7be3ad456248509cb3584dad8683b0b11a51
ArturCSegat <arturcamerasegat@gmail.com>
2023-09-08 16:22:57 -0300

    fixed get_server_socket_or_die(const char * server_ip) in network_utils.h

--- headers/network_utils.h
+++ headers/network_utils.h
@@ -7,7 +7,7 @@ void *get_in_addr(struct sockaddr *sa);
 
 struct addrinfo * get_sock_info(const char * ip);
 
-int get_server_sock_or_die(); 
+int get_server_sock_or_die(const char * server_ip); 
 int get_listening_sock_or_die(); 
 
 #endif
